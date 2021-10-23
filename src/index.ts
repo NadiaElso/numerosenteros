fbbc349f10ff478d5fea4de8b58ee303f069a017
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2021-10-23 10:34:25 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let primernumero = Number(prompt("Ingrese un  primer número entero"));
+let segundonumero = Number(prompt("ingrese un  segundo número entero"));
+let numeroactual, suma: number;
+let numeroactual = primernumero;
+suma = 0;
+
+while (numeroactual <= segundonumero) {
+  suma = suma + numeroactual;
+  actual++;
+}
+console.log("El resultado de la suma es:", suma);
